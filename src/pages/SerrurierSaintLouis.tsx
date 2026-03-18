3a0676e9d2d2f770c847b34c7ac5eacd10fe32be
Schems Kerdoncuff <schems.kerdoncuff@gmail.com>
2026-03-18 09:19:42 +0000

    Ajoute page SerrurierSaintLouis

---
+++ src/pages/SerrurierSaintLouis.tsx
@@ -0,0 +1,4 @@
+import { saintLouisData } from "@/data/saint-louis";
+import VillePageLayout from "@/components/ville/VillePageLayout";
+const SerrurierSaintLouis = () => <VillePageLayout data={saintLouisData} />;
+export default SerrurierSaintLouis;
