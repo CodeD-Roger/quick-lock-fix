f6ad2b2943cb6dd3fd8cd257811cb67d497861a2
Schems Kerdoncuff <schems.kerdoncuff@gmail.com>
2026-03-18 09:19:48 +0000

    Ajoute page SerrurierWittenheim

---
+++ src/pages/SerrurierWittenheim.tsx
@@ -0,0 +1,4 @@
+import { wittenheimData } from "@/data/wittenheim";
+import VillePageLayout from "@/components/ville/VillePageLayout";
+const SerrurierWittenheim = () => <VillePageLayout data={wittenheimData} />;
+export default SerrurierWittenheim;
