1289d809f9ffc5ed5a15a3a36a2ffdbbde74eb07
Schems Kerdoncuff <schems.kerdoncuff@gmail.com>
2026-03-18 09:20:05 +0000

    Ajoute page SerrurierRixheim

---
+++ src/pages/SerrurierRixheim.tsx
@@ -0,0 +1,4 @@
+import { rixheimData } from "@/data/rixheim";
+import VillePageLayout from "@/components/ville/VillePageLayout";
+const SerrurierRixheim = () => <VillePageLayout data={rixheimData} />;
+export default SerrurierRixheim;
