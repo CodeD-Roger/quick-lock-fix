a52fd6ea390fb3d02c811ccfea340bf6f9bb6b16
Schems Kerdoncuff <schems.kerdoncuff@gmail.com>
2026-03-18 09:19:58 +0000

    Ajoute page SerrurierKingersheim

---
+++ src/pages/SerrurierKingersheim.tsx
@@ -0,0 +1,4 @@
+import { kingersheimData } from "@/data/kingersheim";
+import VillePageLayout from "@/components/ville/VillePageLayout";
+const SerrurierKingersheim = () => <VillePageLayout data={kingersheimData} />;
+export default SerrurierKingersheim;
