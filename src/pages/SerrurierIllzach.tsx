0d6611d90c9cc6cf0d4ec84c3c0823fc6b02a6ca
Schems Kerdoncuff <schems.kerdoncuff@gmail.com>
2026-03-18 09:19:53 +0000

    Ajoute page SerrurierIllzach

---
+++ src/pages/SerrurierIllzach.tsx
@@ -0,0 +1,4 @@
+import { illzachData } from "@/data/illzach";
+import VillePageLayout from "@/components/ville/VillePageLayout";
+const SerrurierIllzach = () => <VillePageLayout data={illzachData} />;
+export default SerrurierIllzach;
