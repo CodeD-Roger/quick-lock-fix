51243a41d10e777851bbdb32d327c3b3dbd9c5cd
Schems Kerdoncuff <schems.kerdoncuff@gmail.com>
2026-03-18 09:20:21 +0000

    Ajoute page SerrurierGuebwiller

---
+++ src/pages/SerrurierGuebwiller.tsx
@@ -0,0 +1,4 @@
+import { guebwillerData } from "@/data/guebwiller";
+import VillePageLayout from "@/components/ville/VillePageLayout";
+const SerrurierGuebwiller = () => <VillePageLayout data={guebwillerData} />;
+export default SerrurierGuebwiller;
