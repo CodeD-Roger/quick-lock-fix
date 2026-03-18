1cefb755140eaa5c8dd544e6285505f565ee98ca
Schems Kerdoncuff <schems.kerdoncuff@gmail.com>
2026-03-18 09:20:11 +0000

    Ajoute page SerrurierRiedisheim

---
+++ src/pages/SerrurierRiedisheim.tsx
@@ -0,0 +1,4 @@
+import { riedisheimData } from "@/data/riedisheim";
+import VillePageLayout from "@/components/ville/VillePageLayout";
+const SerrurierRiedisheim = () => <VillePageLayout data={riedisheimData} />;
+export default SerrurierRiedisheim;
