21d393bb6e5c8ab05d0045d215098c27bbebaba1
Schems Kerdoncuff <schems.kerdoncuff@gmail.com>
2026-03-18 09:20:36 +0000

    Ajoute 8 nouvelles routes villes Haut-Rhin dans App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,14 @@ import Index from "./pages/Index";
 import MentionsLegales from "./pages/MentionsLegales";
 import SerrurierMulhouse from "./pages/SerrurierMulhouse";
 import SerrurierColmar from "./pages/SerrurierColmar";
+import SerrurierSaintLouis from "./pages/SerrurierSaintLouis";
+import SerrurierWittenheim from "./pages/SerrurierWittenheim";
+import SerrurierIllzach from "./pages/SerrurierIllzach";
+import SerrurierKingersheim from "./pages/SerrurierKingersheim";
+import SerrurierRixheim from "./pages/SerrurierRixheim";
+import SerrurierRiedisheim from "./pages/SerrurierRiedisheim";
+import SerrurierCernay from "./pages/SerrurierCernay";
+import SerrurierGuebwiller from "./pages/SerrurierGuebwiller";
 import NotFound from "./pages/NotFound";
 import CookieBanner from "./components/CookieBanner";
 
@@ -24,6 +32,14 @@ const App = () => (
           <Route path="/mentions-legales" element={<MentionsLegales />} />
           <Route path="/serrurier-mulhouse" element={<SerrurierMulhouse />} />
           <Route path="/serrurier-colmar" element={<SerrurierColmar />} />
+          <Route path="/serrurier-saint-louis" element={<SerrurierSaintLouis />} />
+          <Route path="/serrurier-wittenheim" element={<SerrurierWittenheim />} />
+          <Route path="/serrurier-illzach" element={<SerrurierIllzach />} />
+          <Route path="/serrurier-kingersheim" element={<SerrurierKingersheim />} />
+          <Route path="/serrurier-rixheim" element={<SerrurierRixheim />} />
+          <Route path="/serrurier-riedisheim" element={<SerrurierRiedisheim />} />
+          <Route path="/serrurier-cernay" element={<SerrurierCernay />} />
+          <Route path="/serrurier-guebwiller" element={<SerrurierGuebwiller />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
